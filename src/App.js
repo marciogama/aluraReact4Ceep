68a240101f5a2359e9d695c32e78231c41d80317
marciogama <marciogama2000@gmail.com>
2020-11-16 14:17:45 -0300

    componetizando e colocando estilos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from "react";
+import ListaDeNotas from './components/ListaDeNotas';
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import "./assets/index.css";
 
-function App() {
-  return (
-    <h1>Meu app react com apenas o que importa para o projeto</h1>
-  );
+class App extends Component {
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
